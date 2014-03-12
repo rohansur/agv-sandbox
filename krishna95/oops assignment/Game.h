5c97d3b066357b50b62430409c11ae6be13764b8
krishna95 <krishna.bagadia2@gmail.com>
2014-03-12 07:36:42 -0700

    Update Game.h

--- krishna95/oops assignment/Game.h
+++ krishna95/oops assignment/Game.h
@@ -2,19 +2,25 @@
 #include <math.h>
 #include <stdlib.h>
 #include <new>
+#include <stdio.h>
+#include <highgui.h>
+#include <math.h>
+#include <cv.h>
+#include <cxcore.h> 
+
+
 //g++ game.cpp 'pkg-config opencv --cflags --libs'
 using namespace std;
 
 class Game{
 	private:
 	int dice;
-	IplImage *img;
-	Player *winner;
 	public:
+	Player *winner;
+	IplImage *img;
 	Player *players;
 	int num_players;
 	void numofplayers(int x);
 	void  setplayer( Player player,int Id);
-	void setting(Player *players);
-	void callplayer(int id);
-}
+	void setting(Player* players);
+};

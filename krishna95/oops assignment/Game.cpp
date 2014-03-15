98cfac39907eeed9b36bcc37c71a09b2b3dd892b
krishna95 <krishna.bagadia2@gmail.com>
2014-03-15 06:47:31 -0700

    Update Game.cpp
    final one

--- krishna95/oops assignment/Game.cpp
+++ krishna95/oops assignment/Game.cpp
@@ -1,16 +1,23 @@
 #include "Game.h"
 
 void Game::numofplayers(int x){
+		cout<<"starting to create an array of players"<<endl;
 		players=new Player[x];
-}
-void Game::setplayer( Player player,int Id){
-	player.id=Id;
-	player.pos.x=0;
-	player.pos.y=0;
-}
-void Game::setting(Player *players){
-	numofplayers(num_players);
-	for (int i=0;i<num_players;i++)
-		setplayer(players[i],i);
-}
-	
+		cout<<"array of players created"<<endl;
+	}
+	void  Game::setplayer(Player* player,int Id,Image image){
+			cout<<"initialising the values for player"<<endl;
+			player->id=Id;
+			player->pos.x=0;
+			player->pos.y=0;
+			player->score=0;
+			player->_count=image.count;
+			
+		}
+	void Game::setting(Image image){
+		cout<<"enter the setting zone"<<endl;
+		numofplayers(num_players);
+		for (int i=0;i<num_players;i++)
+			setplayer(&players[i],i,image);
+		cout<<"initialising done"<<endl;
+	}
